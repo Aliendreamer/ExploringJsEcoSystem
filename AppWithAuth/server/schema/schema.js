047ba37e1c2ab9ba685801bc19075035d5a534d7
Aliendreamer <dreamingman83@gmail.com>
2019-09-22 12:12:03 +0300

    Small refactoring and renaming of the repo and folders

--- AppWithAuth/server/schema/schema.js
+++
@@ -1,9 +0,0 @@
-const graphql = require('graphql');
-const { GraphQLSchema } = graphql;
-const  mutation=require('./mutations');
-const RootQueryType = require('./types/root_query_type');
-
-module.exports = new GraphQLSchema({
-  query: RootQueryType,
-  mutation:mutation
-});

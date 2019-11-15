dcae8d9be1a14cbdf73034ed8baf17a83377b79a
Aliendreamer <dreamingman83@gmail.com>
2019-11-15 23:27:30 +0200

    found why redis was not working

--- AdvancedNodeStarter/routes/blogRoutes.js
+++ AdvancedNodeStarter/routes/blogRoutes.js
@@ -14,7 +14,18 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis =require("redis"); 
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    const util = require("util");
+    client.get = util.promisify(client.get);
+    const cashedBlogs = await client.get(req.user.id);
+    if (cashedBlogs){
+      console.log("from cache");
+      return res.send(JSON.parse(cashedBlogs));
+    }
     const blogs = await Blog.find({ _user: req.user.id });
+    client.set(req.user.id,JSON.stringify(blogs));
 
     res.send(blogs);
   });

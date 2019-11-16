fdddf0354a7eeafdc7bf0c0f149d30b037249b36
Aliendreamer <dreamingman83@gmail.com>
2019-11-16 17:03:19 +0200

    up to section 5 caching done

--- AdvancedNodeStarter/routes/blogRoutes.js
+++ AdvancedNodeStarter/routes/blogRoutes.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-
+const cleanCache= require("../middlewares/cleanCache");
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
@@ -14,23 +14,15 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis =require("redis"); 
-    const redisUrl = 'redis://127.0.0.1:6379';
-    const client = redis.createClient(redisUrl);
-    const util = require("util");
-    client.get = util.promisify(client.get);
-    const cashedBlogs = await client.get(req.user.id);
-    if (cashedBlogs){
-      console.log("from cache");
-      return res.send(JSON.parse(cashedBlogs));
-    }
-    const blogs = await Blog.find({ _user: req.user.id });
-    client.set(req.user.id,JSON.stringify(blogs));
-
+   
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key:req.user.id
+    });
+ 
     res.send(blogs);
   });
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
+  app.post('/api/blogs', requireLogin,cleanCache, async (req, res) => {
     const { title, content } = req.body;
 
     const blog = new Blog({
---
+++ AdvancedNodeStarter/services/cache.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+
+const redisUrl ='redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.hget = util.promisify(client.hget);
+const exec = mongoose.Query.prototype.exec;
+
+
+mongoose.Query.prototype.cache = function (options={}){
+    this.useCache = true;
+    this.hashKey = JSON.stringify(options.key || '');
+    return this;
+}
+
+
+mongoose.Query.prototype.exec = async function(){
+  
+  if(!this.useCache){
+    return exec.apply(this,arguments);
+  }
+  const key = Object.assign({},this.getQuery(),{
+       collection:this.mongooseCollection.name
+   });
+   const redisKey = JSON.stringify(key);
+   const cacheValue = await client.hget(redisKey);
+   if(cacheValue){
+     const doc = JSON.parse(cacheValue);
+     const cached = Array.isArray(doc)
+     ? doc.map(x=> new this.model(x)) 
+     : new this.model(doc);
+    return  cached;
+   }
+
+   const result = await  exec.apply(this,arguments); 
+   client.hset(this.hashKey,redisKey,JSON.stringify(result),"EX",60);
+   return result;  
+}
+
+
+module.exports = {
+  clearHash(hashKey){
+      client.del(JSON.stringify(hashKey));
+  }
+}

86b58873cabdd9a5fc105db276a9f0217ecd802c
Aliendreamer <dreamingman83@gmail.com>
2019-12-30 19:54:01 +0200

    fixing rotation in both directions

--- reactHooksCourse/reacthooksOne/src/carousel.js
+++ reactHooksCourse/reacthooksOne/src/carousel.js
@@ -6,16 +6,21 @@ const rotationStep = 1;
 const BubbleChart =() => {
     const [changeDate, setChangeDate] = useState(initialState);
     
-    const handleChange = () => {
+    const handleChange = (isLeft) => {
         let currentIndex = slidesDate.indexOf(changeDate);
-        let nextIndex = currentIndex+rotationStep <= slidesDate.length - rotationStep ?  currentIndex+ rotationStep : 0;
+        let nextIndex;
+        if (isLeft) {
+            nextIndex = currentIndex - rotationStep >= 0 ? currentIndex - rotationStep : slidesDate.length-rotationStep;
+        } else {
+            nextIndex = currentIndex + rotationStep <= slidesDate.length - rotationStep ? currentIndex + rotationStep : 0;
+        }
         setChangeDate(slidesDate[nextIndex])
     }
 return (
 <div className="select-date-buttons">
-<button className="caret" onClick={() => handleChange()}>&#60;</button>
+<button className="caret" onClick={() => handleChange(true)}>&#60;</button>
 <button>{changeDate}</button>
-<button className="caret" onClick={() => handleChange()}>&#62;</button>
+<button className="caret" onClick={() => handleChange(false)}>&#62;</button>
 <button className="export-button">&#x2b73;</button>
 </div>
 )

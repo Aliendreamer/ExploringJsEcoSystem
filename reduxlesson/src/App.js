f8dfdea44169e49326ba7a3dc635bca5cca0bcc3
Teo <teodor.g.stefanov@mobiltel.bg>
2020-06-04 22:29:16 +0300

    local state example

--- reduxlesson/src/App.js
+++ reduxlesson/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ComponentOne from "./components/componentOne";
+
+//uncomment if you are trying the props drilling example or reading point 2 of the article
+//and pass it to the app
+//import ComponentOne from "./components/propsDrilling/componentOne";
+import CounterComponent from "./components/localState/counterComponent";
 
 function App() {
   return (
     <div >
-     <ComponentOne/>
+     <CounterComponent/>
     </div>
   );
 }
---
+++ reduxlesson/src/components/localState/counterComponent.js
@@ -0,0 +1,23 @@
+import React,{useState,useEffect} from 'react';
+
+const CounterComponent = ()=>{
+
+   const [fieldValue,setFieldValue]=useState(0);
+   useEffect(()=>{
+
+   },[fieldValue])
+   return(
+      <div style={{marginLeft:"100px", marginTop:"100px"}}>
+      <input type='text' value={fieldValue} label="Current counter" disabled/>
+      <button type='button' onClick={()=>setFieldValue(fieldValue+1)}>
+         Increment
+      </button>
+      <button type='button' onClick={()=>setFieldValue(fieldValue-1>=0?fieldValue-1:0)} >
+         Decrement
+      </button>
+      </div>
+   )
+      
+}
+
+export default CounterComponent;

ace46d93fde67c69a688682d413ee2b8dd248a50
Teo <teodor.g.stefanov@mobiltel.bg>
2020-06-25 13:40:59 +0300

    working on keycloak example

--- graphqlLesson/apolloApp/client/src/index.js
+++ graphqlLesson/apolloApp/client/src/index.js
@@ -6,6 +6,8 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { InMemoryCache, IntrospectionFragmentMatcher } from "apollo-cache-inmemory";
+import {setContext} from "apollo-link-context";
+import {createHttpLink} from "apollo-link-http";
 
 const fragmentMatcher = new IntrospectionFragmentMatcher({
   introspectionQueryResultData: {
@@ -27,7 +29,20 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
     },
   }
 });
+const httpLink = createHttpLink({
+  uri:"/graphql"
+})
+const authLink = (_,{headers}) =>{
+  const token = localStorage.getItem("token");
+  return {
+    headers:{
+      ...headers,
+      authorization:token ?`Bearer ${token}`:""
+    }
+  }
+}
 const client = new ApolloClient({
+  link:authLink.concat(httpLink),
   uri: "http://localhost:5000/graphql",
   cache:new InMemoryCache({ fragmentMatcher })
 });
--- graphqlLesson/apolloApp/index.js
+++ graphqlLesson/apolloApp/index.js
@@ -1,8 +1,26 @@
-const { ApolloServer, gql } = require('apollo-server');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
+const Keycloak = require('keycloak-connect')
+const { KeycloakContext, KeycloakTypeDefs, KeycloakSchemaDirectives } = require('keycloak-connect-graphql')
 
-const server = new ApolloServer({ typeDefs, resolvers });
+
+
+const app = express();
+const keycloak = new Keycloak();
+app.use(graphqlPath, keycloak.middleware());
+const server = new ApolloServer({
+	typeDefs:[KeycloakTypeDefs,typeDefs],
+	 resolvers,
+	 schemaDirectives:KeycloakSchemaDirectives,
+	 context:({req})=>{
+		 return {
+			 kauth:new KeycloakContext({req})
+		 }
+	 }
+});
+server.applyMiddleware({app});
 const PORT = 5000;
 server.listen(PORT, async() => {
 	console.log(`Listening on port '${PORT}'.`);

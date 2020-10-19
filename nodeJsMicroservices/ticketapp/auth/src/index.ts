1a59df06f666c3a092a1c94755ccc69bc39fb8ca
Teo <dreamingman83@gmail.com>
2020-10-19 23:08:16 +0300

    fucking devops bulshit

--- nodeJsMicroservices/ticketapp/auth/src/index.ts
+++ nodeJsMicroservices/ticketapp/auth/src/index.ts
@@ -1,13 +1,15 @@
 import express from "express";
 import {json} from "body-parser";
-
+import {currentUserRouter} from "./routes/current-user";
+import {signUpRouter} from "./routes/signup";
+import { signInRouter} from "./routes/signin";
+import {signOutRouter} from "./routes/signout";
 const app = express();
 app.use(json());
-
-app.get("/api/users/currentuser",(req,res)=>{
-   res.send("works")
-});
-
-app.listen(4000,()=>{
-   console.log("listening at 4000")
+app.use(signInRouter);
+app.use(signUpRouter);
+app.use(signOutRouter);
+app.use(currentUserRouter);
+app.listen(3000,()=>{
+   console.log("listening at 3000")
 })
